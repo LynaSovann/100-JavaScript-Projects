45670c5e38b76b49dc0f720a2fec617c782d9afe
LynaSovann <sovannlyna2004@gmail.com>
2023-10-04 15:13:57 +0700

    pro 2

---
+++ Pro.2-Convert-foot-to-meter/script.js
@@ -0,0 +1,13 @@
+const formElement = document.querySelector("[data-form]");
+const resultText = document.querySelector("[data-result]");
+const inputElement = document.querySelector("[data-input]");
+
+formElement.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const tempValue = inputElement.value * 0.3048;
+  const result = parseFloat(tempValue).toFixed(2);
+  resultText.innerText = `${inputElement.value} ft = ${result} m`;
+  inputElement.value = "";
+}
