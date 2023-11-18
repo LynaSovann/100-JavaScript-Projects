99475e0bde378752f8256cc15d34bf2771028100
LynaSovann <sovannlyna2004@gmail.com>
2023-11-18 07:43:20 +0700

    divided code into each function for each task in pro.11

--- Pro.11-simple-calculator/script.js
+++ Pro.11-simple-calculator/script.js
@@ -4,11 +4,36 @@ const buttonElements = document.querySelectorAll("button");
 Array.from(buttonElements, (btn) => {
   btn.addEventListener("click", () => {
     if (btn.innerText === "C") {
-      inputElement.value = "";
+      clearInput();
     } else if (btn.innerText === "=") {
-      inputElement.value = eval(inputElement.value);
+      calculation();
     } else {
-      inputElement.value += btn.innerText;
+      inputDisplay(btn.innerText);
     }
   });
 });
+
+function clearInput() {
+  inputElement.value = "";
+}
+
+function calculation() {
+  if (inputElement.value === "") {
+    return;
+  }
+  inputElement.value = eval(inputElement.value);
+}
+
+function inputDisplay(arg) {
+  const lastText = inputElement.value.slice(-1);
+  if (
+    (lastText === "+") |
+    (lastText === "-") |
+    (lastText === "*") |
+    (lastText === "/")
+  ) {
+    if ((arg === "+") | (arg === "-") | (arg === "*") | (arg === "/")) return;
+  }
+  inputElement.value += arg;
+  console.log(inputElement.value.slice(-1));
+}
