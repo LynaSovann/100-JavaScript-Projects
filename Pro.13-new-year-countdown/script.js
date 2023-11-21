8831ac0843dc5628b1df37addc44ab51c113c1db
LynaSovann <sovannlyna2004@gmail.com>
2023-11-22 06:32:19 +0700

    count down new year

---
+++ Pro.13-new-year-countdown/script.js
@@ -0,0 +1,31 @@
+const dayElement = document.getElementById("day");
+const hourElement = document.getElementById("hour");
+const minuteElement = document.getElementById("minute");
+const secondElement = document.getElementById("second");
+
+countDown();
+
+function countDown () {
+
+    const newYear = new Date("Jan 1, 2024 00:00:00").getTime();
+    const now = Date.now();
+    const milisecond = newYear - now;
+    const second = Math.floor(milisecond / 1000);
+    const minute = Math.floor(second / 60);
+    const hour = Math.floor(minute / 60);
+    const day = Math.floor(hour / 24);
+
+    const d = day;
+    const h = hour % 24;
+    const m = minute % 60;
+    const s = second % 60;
+
+    dayElement.innerText = d;
+    hourElement.innerText = h;
+    minuteElement.innerText = m;
+    secondElement.innerText = s;
+
+    setTimeout(() => {
+        countDown();
+    }, 1000)
+}
